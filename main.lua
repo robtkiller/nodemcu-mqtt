b67e85501220103d07b498dbdac74c94ae92a235
rob <rob@kilareski.net>
2016-02-21 22:29:04 -0500

    Added mqtt heartbeat loop,fixed mqtt startup sequence

--- main.lua
+++ main.lua
@@ -1,7 +1,18 @@
 --file: main.lua
 
 config = require("config")
-wifi = require("wifi")
+wireless = require("wireless")
+mqtt_client = require("mqtt_client")
 
-wifi.wifi_start(config)
+wireless.wifi_start(config)
+tmr.alarm(1,1000, 1, 
+  function()
+    if wifi.sta.getip()==nil then 
+      print(" Wait to IP address!") 
+    else 
+      print("New IP address is "..wifi.sta.getip()) 
+      tmr.stop(1) mqtt_client.connect(config) 
+    end 
+  end)
+
 
